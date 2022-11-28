65d5fc0346f9caf60df48f57650678326d35f8d9
Michaela Hanková <49369316+hihaha19@users.noreply.github.com>
2022-11-28 09:22:36 +0100

    Add files via upload

---
+++ javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"version2","l":"CreateCSV"},{"p":"version2","l":"Indexer"},{"p":"version2","l":"Main"},{"p":"version2","l":"ReadIndex"}];updateSearchResults();
